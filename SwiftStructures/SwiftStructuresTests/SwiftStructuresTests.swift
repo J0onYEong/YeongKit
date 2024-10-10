cc7cf6f38dc65e3f565045db78d5e3d47e75f844
J0onYEong <cjy2134@naver.com>
2024-10-10 22:11:16 +0900

    [SS-001] Feat,  테스크 케이스 추가

--- SwiftStructures/SwiftStructuresTests/SwiftStructuresTests.swift
+++ SwiftStructures/SwiftStructuresTests/SwiftStructuresTests.swift
@@ -10,27 +10,59 @@ import XCTest
 
 class SwiftStructuresTests: XCTestCase {
 
-    func testRBTreeMakeBalanceBST() throws {
+    func testRBTreeMakeBalanceBST20() throws {
         
-        let rbTree = RBTree<Int>()
-        let testElements = [56, 10, 3, 61, 49, 97, 9, 72, 40, 94, 67, 2, 98, 5, 71, 57, 15, 36, 87, 99]
+        let testCases = [
+            [71, 3, 31, 10, 24, 54, 32, 22, 96, 68, 62, 11, 97, 67, 61, 52, 43, 49, 59, 80],
+            [40, 70, 45, 53, 2, 50, 83, 7, 47, 42, 4, 65, 46, 38, 95, 18, 79, 6, 1, 27],
+            [44, 30, 82, 76, 87, 15, 41, 26, 51, 64, 20, 39, 19, 90, 16, 94, 72, 99, 84, 78],
+        ]
         
-        let expactedTreeHeight = 5
-        
-        testElements.forEach { element in
+        for testCase in testCases {
+            let expactedTreeHeightRange = 5...6
             
-            do {
-                try rbTree.append(element)
+            let rbTree = RBTree<Int>()
+            
+            testCase.forEach { element in
+                
+                do {
+                    try rbTree.append(element)
+                } catch {
+                    XCTFail(error.localizedDescription)
+                }
+                
                 rbTree.printTree()
-                print("------------")
-            } catch {
-                XCTFail(error.localizedDescription)
             }
+            XCTAssertTrue((expactedTreeHeightRange).contains(rbTree.height))
+            print("------------")
         }
+    }
+    
+    func testRBTreeMakeBalanceBST40() throws {
         
-        rbTree.printTree()
-        
-        XCTAssertEqual(rbTree.height, expactedTreeHeight)
+        let testCases = [
+            [48, 18, 47, 62, 66, 15, 28, 33, 94, 49, 3, 76, 37, 38, 40, 68, 78, 11, 92, 36,
+            69, 4, 95, 53, 23, 12, 81, 88, 44, 32, 46, 91, 14, 74, 80, 86, 22, 25, 39, 1]
+        ]
+    
+        for testCase in testCases {
+            let expactedTreeHeightRange = 6...7
+            
+            let rbTree = RBTree<Int>()
+            
+            testCase.forEach { element in
+                
+                do {
+                    try rbTree.append(element)
+                } catch {
+                    XCTFail(error.localizedDescription)
+                }
+                
+                rbTree.printTree()
+            }
+            XCTAssertTrue((expactedTreeHeightRange).contains(rbTree.height))
+            print("------------")
+        }
     }
     
     func testDuplicateElement() {
