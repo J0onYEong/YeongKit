6ed88218156f9e59ece598c9ec9d107339a1282f
J0onYEong <cjy2134@naver.com>
2024-10-10 19:01:14 +0900

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Swift structures by junios
+
+## Milestone
+
+- [ ] Red-black tree 
+- [ ] Thread-safe dictionary 
---
+++ SwiftStructures/SwiftStructures/SwiftStructures.h
@@ -0,0 +1,18 @@
+//
+//  SwiftStructures.h
+//  SwiftStructures
+//
+//  Created by choijunios on 10/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftStructures.
+FOUNDATION_EXPORT double SwiftStructuresVersionNumber;
+
+//! Project version string for SwiftStructures.
+FOUNDATION_EXPORT const unsigned char SwiftStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStructures/PublicHeader.h>
+
+
