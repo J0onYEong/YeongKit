29a28ddf44a115fadc425cf710115ddc64965c9e
J0onYEong <cjy2134@naver.com>
2024-10-13 01:25:36 +0900

    [SS-004] HashMap 삽입삭제 구현

--- SwiftStructures/SwiftStructures/HashMap/HashMap.swift
+++ SwiftStructures/SwiftStructures/HashMap/HashMap.swift
@@ -7,18 +7,51 @@
 
 import Foundation
 
-public class HashMap<Key, Value> where Key: Hashable {
+public class HashMap<Key, Value> where Key: Hashable & Comparable {
     
-    typealias Store = LockedDictionary<Key, Value>
+    typealias KeyStore = RBTree<Key>
     
-    private let store: Store = .init()
+    private var dictionary: [Key: Value] = .init()
+    private let keyStore: KeyStore = .init()
+    
+    private let lock: NSLock = .init()
     
     public subscript(key: Key) -> Value? {
         get {
-            return store[key]
+            defer {
+                lock.unlock()
+            }
+            lock.lock()
+            
+            return dictionary[key]
         }
         set(newValue) {
-            store[key] = newValue
+            defer {
+                lock.unlock()
+            }
+            lock.lock()
+            
+            if dictionary[key] == nil {
+                // key isn't exists in dictionary
+                try! keyStore.append(key)
+            }
+            
+            dictionary[key] = newValue
         }
+    }
+    
+    public func remove(_ key: Key) {
+        defer {
+            lock.unlock()
+        }
+        lock.lock()
+        
+        do {
+            try keyStore.remove(key)
+        } catch {
+            print("\(HashMap.self): " + error.localizedDescription)
+        }
+        
+        dictionary.removeValue(forKey: key)
     }
 }
---
+++ SwiftStructures/SwiftStructuresTests/HashMapTests.swift
@@ -0,0 +1,59 @@
+//
+//  HashMapTests.swift
+//  SwiftStructuresTests
+//
+//  Created by choijunios on 10/13/24.
+//
+
+import XCTest
+@testable import SwiftStructures
+
+class HashMapTests: XCTestCase {
+    
+    func testInsertionAndRetrieval() {
+        // Given
+        let hashMap = HashMap<Int, String>()
+        
+        // When
+        hashMap[1] = "One"
+        hashMap[2] = "Two"
+        hashMap[3] = "Three"
+        
+        // Then
+        XCTAssertEqual(hashMap[1], "One", "Value for key 1 should be 'One'")
+        XCTAssertEqual(hashMap[2], "Two", "Value for key 2 should be 'Two'")
+        XCTAssertEqual(hashMap[3], "Three", "Value for key 3 should be 'Three'")
+    }
+    
+    func testRemoveKey() {
+        // Given
+        let hashMap = HashMap<Int, String>()
+        
+        // Insert some values
+        hashMap[1] = "One"
+        hashMap[2] = "Two"
+        hashMap[3] = "Three"
+        
+        // When
+        hashMap.remove(2)  // Remove the key 2
+        
+        // Then
+        XCTAssertNil(hashMap[2], "Value for key 2 should be nil after removal")
+        XCTAssertEqual(hashMap[1], "One", "Value for key 1 should still be 'One'")
+        XCTAssertEqual(hashMap[3], "Three", "Value for key 3 should still be 'Three'")
+    }
+    
+    func testInsertionOfExistingKey() {
+        // Given
+        let hashMap = HashMap<Int, String>()
+        
+        // Insert a value
+        hashMap[1] = "One"
+        
+        // When
+        hashMap[1] = "Updated One"  // Update the value for key 1
+        
+        // Then
+        XCTAssertEqual(hashMap[1], "Updated One", "Value for key 1 should be updated to 'Updated One'")
+    }
+}
